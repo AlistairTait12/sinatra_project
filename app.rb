b273871f6ec317656cd0b5888b19f33a879c3909
AlistairTait12 <alistair.tait1@gmail.com>
2021-02-09 17:42:57 +0000

    beginning posted params

--- app.rb
+++ app.rb
@@ -8,7 +8,13 @@ get '/another' do
   "another one bites the dust"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ['Monty', 'Jasper', 'Clive'].sample
   erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:named)
 end
